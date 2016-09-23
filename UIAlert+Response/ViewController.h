f3897a151f6b43b23e8ecf20b268bc2ea9693db3
Lili <lili2011050508@163.com>
2016-09-23 11:47:23 +0800

    Initial Commit

---
+++ UIAlert+Response/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UIAlert+Response
+//
+//  Created by LiliEhuu on 16/9/23.
+//  Copyright © 2016年 LiliEhuu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ UIAlert+Response/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  UIAlert+Response
+//
+//  Created by LiliEhuu on 16/9/23.
+//  Copyright © 2016年 LiliEhuu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
